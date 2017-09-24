ee4b376e374782e3201c6433172d5114a8cfc59d
Edouard Murat <edouardmurat1@gmail.com>
2017-09-24 15:32:26 -0400

    Browser back button closes modal

--- js/agency.js
+++ js/agency.js
@@ -35,4 +35,33 @@
     }
   });
 
+  // cause back button to close bootstrap modal windows
+  $('div.modal').on('show.bs.modal', function() {
+      var modal = this;
+      var hash = modal.id;
+      window.location.hash = hash;
+      window.onhashchange = function() {
+          if (!location.hash){
+              $(modal).modal('hide');
+          }
+      }
+  });
+
+  $('div.modal').on('hidden.bs.modal', function() {
+      var hash = this.id;
+      history.replaceState('', document.title, window.location.pathname);
+  });
+
+  // when close button clicked simulate back
+  $('div.modal button.close').on('click', function(){
+      window.history.back();
+  })
+
+  // when esc pressed when modal open simulate back
+  $('div.modal').keyup(function(e) {
+      if (e.keyCode == 27){
+          window.history.back();          
+      }
+  });
+
 })(jQuery); // End of use strict

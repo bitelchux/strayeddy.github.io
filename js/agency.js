01726eaf88242faf278b01857532fa93b6363bdc
Edouard Murat <edouardmurat1@gmail.com>
2017-10-05 16:06:57 -0400

    Add images for news + Add dynamic news modal

--- js/agency.js
+++ js/agency.js
@@ -37,31 +37,41 @@
 
   // cause back button to close bootstrap modal windows
   $('div.modal').on('show.bs.modal', function() {
-      var modal = this;
-      var hash = modal.id;
-      window.location.hash = hash;
-      window.onhashchange = function() {
-          if (!location.hash){
-              $(modal).modal('hide');
-          }
+    var modal = this;
+    var hash = modal.id;
+    window.location.hash = hash;
+    window.onhashchange = function() {
+      if (!location.hash){
+        $(modal).modal('hide');
       }
+    }
   });
 
   $('div.modal').on('hidden.bs.modal', function() {
-      var hash = this.id;
-      history.replaceState('', document.title, window.location.pathname);
+    var hash = this.id;
+    history.replaceState('', document.title, window.location.pathname);
   });
 
   // when close button clicked simulate back
   $('div.modal button.close').on('click', function(){
-      window.history.back();
+    window.history.back();
   })
 
   // when esc pressed when modal open simulate back
   $('div.modal').keyup(function(e) {
-      if (e.keyCode == 27){
-          window.history.back();          
-      }
+    if (e.keyCode == 27){
+      window.history.back();          
+    }
+  });
+
+  $(document).ready(function(){
+    $('a[href="#newsModal"]').click(function(e) {
+      var modalId = $(this).attr('data-news-id');
+      var url = 'news/' + modalId + '.html';
+      $('#newsModal').load(url,function(result){
+        $('#newsModal').modal({show:true});
+      });
+    });
   });
 
 })(jQuery); // End of use strict

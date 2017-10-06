8ea0ea7631978bb6112c2cfc8bbef7c4f24d6254
Edouard Murat <edouardmurat1@gmail.com>
2017-10-06 17:31:33 -0400

    Added dropdown to services

--- js/agency.js
+++ js/agency.js
@@ -6,6 +6,7 @@
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
       if (target.length) {
         $('html, body').animate({
           scrollTop: (target.offset().top - 54)
@@ -14,6 +15,18 @@
       }
     }
   });
+
+  $('#servicesDropdown').on('shown.bs.dropdown', function () {
+    var target = $('#services');
+    target = target.length ? target : $('[name=services]');
+
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: (target.offset().top - 54)
+      }, 1000, "easeInOutExpo");
+      return false;
+    }
+  })
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {

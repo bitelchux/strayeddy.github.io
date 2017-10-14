06e40986a41ea5bc7ea1d23ce0270c4470c43468
Edouard Murat <edouardmurat1@gmail.com>
2017-10-14 19:53:05 -0400

    Add script to combine all js and css files & minify them

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# combine css and js files
+cat vendor/bootstrap/css/bootstrap.css css/agency.css vendor/font-awesome/css/font-awesome.css css/fonts.css > css/all.css
+cat js/vendors.min.js js/agency.js > js/all.js
+
+# minify css and js
+minify -o css/all.min.css css/all.css
+minify -o js/all.min.js js/all.js
